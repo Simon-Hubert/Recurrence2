d498f72b19d710d6baa5c4907fb614aca021cf6d
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-01-22 17:08:33 +0100

    [END]

--- Assets/Scripts/UI/BackgroundOnClick.cs
+++ Assets/Scripts/UI/BackgroundOnClick.cs
@@ -1,14 +1,40 @@
-using System.Collections;
+using UnityEngine.EventSystems;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BackgroundOnClick : MonoBehaviour
+public class BackgroundOnClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] List<Sprite> spritesTop;
-    [SerializeField] List<Sprite> spriteBottom;
+    [SerializeField] List<Sprite> spritesBottom;
+    [SerializeField] Image _background;
+    [SerializeField] ParticleSystem _particleSystem;
 
-    private void OnMouseDown()
+    bool _isPressed = false;
+
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        _isPressed = true;
+        changeBottom();
+        _particleSystem.Play();
+        AudioManager.instance.PlayRand();
     }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isPressed &= false;
+        changeTop();
+    }
+
+    void changeTop()
+    {
+        int rand = Random.Range(0, spritesTop.Count);
+        _background.sprite = spritesTop[rand];
+    }
+    void changeBottom()
+    {
+        int rand = Random.Range(0, spritesBottom.Count);
+        _background.sprite = spritesBottom[rand];
+    }
+
 }
