69c6a2f1daec2fc58bea73e235501ee29fc7ffe0
Enzo Bossé <117042107+Bl0bli@users.noreply.github.com>
2024-01-22 16:35:02 +0100

    [ADD] fx

---
+++ Assets/Scripts/UI/BackgroundOnClick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundOnClick : MonoBehaviour
+{
+    [SerializeField] List<Sprite> spritesTop;
+    [SerializeField] List<Sprite> spriteBottom;
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
